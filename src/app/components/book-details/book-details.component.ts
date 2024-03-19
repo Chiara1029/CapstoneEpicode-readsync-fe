81a66076e640ac916658abdbcc29c0b0e9909031
Chiara Puleio <chiarapuleio@yahoo.it>
2024-03-20 00:10:48 +0100

    review post created and tested

--- src/app/components/book-details/book-details.component.ts
+++ src/app/components/book-details/book-details.component.ts
@@ -12,6 +12,8 @@ import { Review, ReviewResponse } from 'src/app/interfaces/review';
 export class BookDetailsComponent implements OnInit {
   book!: Book;
   reviews: Review[];
+  newReview: any = {};
+  userId!: string;
 
   constructor(private route: ActivatedRoute, private http: HttpClient) {
     this.reviews = [];
@@ -27,6 +29,7 @@ export class BookDetailsComponent implements OnInit {
         console.error('ISBN code not found in URL');
       }
     });
+    const userId = this.fetchUserId();
   }
 
   getBookDetails(isbnCode: string): void {
@@ -69,13 +72,54 @@ export class BookDetailsComponent implements OnInit {
       })
       .subscribe(
         (reviews) => {
-          console.log(this.reviews);
-          console.log(reviews);
           this.reviews = reviews.content;
         },
         (error) => {
           console.error('Error fetching reviews:', error);
         }
       );
+  }
+
+  submitReview() {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      console.error('Token not found');
+      return;
+    }
+
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+
+    const reviewBody = {
+      ...this.newReview,
+      bookIsbn: this.book.isbnCode,
+      userId: this.userId,
+    };
+
+    this.http
+      .post('http://localhost:3001/reviews', reviewBody, { headers })
+      .subscribe(
+        (data: any) => {
+          this.reviews.push(data);
+          this.newReview = {};
+        },
+        (error) => {
+          console.error('Error submitting review:', error);
+        }
+      );
+  }
+
+  fetchUserId() {
+    const token = localStorage.getItem('token');
+    const headers = { Authorization: `Bearer ${token}` };
+    this.http.get<any>('http://localhost:3001/users/me', { headers }).subscribe(
+      (response) => {
+        this.userId = response.toString();
+      },
+      (error) => {
+        console.error('Error fetching user id:', error);
+      }
+    );
   }
 }
