5bf2f228c09afde6749d5a13800219b64445c971
Chiara Puleio <chiarapuleio@yahoo.it>
2024-03-19 18:38:06 +0100

    welcome message in homepage(WIP)

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from '../../interfaces/book';
 import { BookService } from '../../services/book.service';
+import { HttpClient } from '@angular/common/http';
+import { User, UserResponse } from 'src/app/interfaces/user';
 
 @Component({
   selector: 'app-home',
@@ -9,11 +11,14 @@ import { BookService } from '../../services/book.service';
 })
 export class HomeComponent implements OnInit {
   books!: Book[];
+  userId!: string;
+  user!: UserResponse;
 
-  constructor(private bookService: BookService) {}
+  constructor(private bookService: BookService, private http: HttpClient) {}
 
   ngOnInit(): void {
     this.loadBooks();
+    this.fetchUserId();
   }
 
   loadBooks() {
@@ -25,5 +30,35 @@ export class HomeComponent implements OnInit {
         console.log('Error fetching books:', error);
       }
     );
+  }
+
+  fetchUserId() {
+    const token = localStorage.getItem('token');
+    const headers = { Authorization: `Bearer ${token}` };
+    this.http.get<any>('http://localhost:3001/users/me', { headers }).subscribe(
+      (response) => {
+        this.userId = response.toString();
+        this.fetchUserDetails();
+      },
+      (error) => {
+        console.error('Error fetching user id:', error);
+      }
+    );
+  }
+
+  fetchUserDetails() {
+    const headers = {
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    };
+    this.http
+      .get<any>(`http://localhost:3001/users/${this.userId}`, { headers })
+      .subscribe(
+        (response) => {
+          this.user = response;
+        },
+        (error) => {
+          console.error('Error fetching user details:', error);
+        }
+      );
   }
 }
