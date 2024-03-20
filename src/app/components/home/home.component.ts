0bae924b0a68a5b5351df695243c5cabea73b633
Chiara Puleio <chiarapuleio@yahoo.it>
2024-03-20 18:43:24 +0100

    delete btn (only ADMIN) created and tested

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,6 +3,7 @@ import { Book } from '../../interfaces/book';
 import { BookService } from '../../services/book.service';
 import { HttpClient } from '@angular/common/http';
 import { User, UserResponse } from 'src/app/interfaces/user';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -13,12 +14,30 @@ export class HomeComponent implements OnInit {
   books!: Book[];
   userId!: string;
   user!: UserResponse;
+  isLoggedIn!: boolean;
+  userRole: string = '';
 
-  constructor(private bookService: BookService, private http: HttpClient) {}
+  constructor(
+    private authSrv: AuthService,
+    private bookService: BookService,
+    private http: HttpClient
+  ) {
+    this.authSrv.isLoggedIn.subscribe((res) => {
+      console.log(res);
+      this.isLoggedIn = res;
+    });
+  }
 
   ngOnInit(): void {
     this.loadBooks();
     this.fetchUserId();
+    this.authSrv.isLoggedIn.subscribe((res) => {
+      console.log(res);
+      this.isLoggedIn = res;
+      if (this.isLoggedIn) {
+        this.fetchUserId();
+      }
+    });
   }
 
   loadBooks() {
@@ -55,9 +74,28 @@ export class HomeComponent implements OnInit {
       .subscribe(
         (response) => {
           this.user = response;
+          this.userRole = response.userRole;
         },
         (error) => {
           console.error('Error fetching user details:', error);
+        }
+      );
+  }
+
+  delete(book: any) {
+    const isbnCode = book.isbnCode;
+    const token = localStorage.getItem('token');
+    const headers = { Authorization: `Bearer ${token}` };
+
+    this.http
+      .delete<any>(`http://localhost:3001/books/${isbnCode}`, { headers })
+      .subscribe(
+        () => {
+          this.books = this.books.filter((i) => i.isbnCode !== isbnCode);
+          alert('The book has been successfully deleted.');
+        },
+        (error) => {
+          console.error('Error deleting this book:', error);
         }
       );
   }
